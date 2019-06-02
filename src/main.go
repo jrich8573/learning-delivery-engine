c2e0a7a846c54cd15f20a268a8c0a8011ab0714b
Jason Rich <jasonrich85@gmail.com>
2019-06-01 22:23:41 -0400

    added user handler and id func

--- src/main.go
+++ src/main.go
@@ -1,8 +1,31 @@
 package main
 
-import ("net/http")
-import ("github.com/gin-gonic/contrib/static")
-import ("github.com/gin-gonic/gin")
+import (
+        //"encoding/json"
+        //"errors"
+        //"fmt"
+        //"log"
+        "net/http"
+        //"os"
+        "strconv"
+        "github.com/gin-gonic/contrib/static"
+        "github.com/gin-gonic/gin"
+)
+
+
+type User struct {
+    ID int `json: "id" binding:"required"`
+    FirstName string `json: "firstname"`
+    MiddleName string `json: "middlename"`
+    LastName string `json: "lastname"`
+    Organization []string `json: "organization"`
+    Logins int `json: "logins"` //count the number if logins
+}
+
+var users = []User{
+    // ToDo: build a scalable users arrary
+}
+
 
 
 func main(){
@@ -22,4 +45,31 @@ func main(){
     // Start and run the server
     router.Run(":3000")
 
+     // Our API will consit of just two and possibly more routes
+     api.GET("/users", userHandler)
+     api.POST("/users/:userID", getUsers)
+}
+     // JokeHandler retrieves a list of available
+func userHandler(c *gin.Context){
+        c.Header("Content-Type", "application/json")
+        c.JSON(http.StatusOK, gin.H{
+            "message":"User handler is not implemented yet",
+        })
+}
+
+// user handler to retrieve valid users
+func getUsers(c *gin.Context){
+    //check for a valid user-id
+    if userid, err := strconv.Atoi(c.Param("userID")); err == nil {
+        //find user and increment logins
+        for i :=0; i < len(users); i++ {
+            if users[i].ID == userid {
+              users[i].Logins = users[i].Logins + 1
+            }
+        }
+        c.JSON(http.StatusOK, &users)
+    } else {
+        // new user
+        c.AbortWithStatus(http.StatusNotFound)
+      }
 }
