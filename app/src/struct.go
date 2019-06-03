6a2947c95a730fa488dc96d8f665e7653a7bc0b4
Jason Rich <jasonrich85@gmail.com>
2019-06-02 22:21:11 -0400

    added struct.go file

---
+++ app/src/struct.go
@@ -0,0 +1,35 @@
+package main
+
+type registerPayload struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type loginPayload struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type user struct {
+	ID           int      `json:"id"`
+	Username     string   `json:"username"`
+	Firstname    string   `json:"firstname"`
+	Middlename   string   `json:"middlename"`
+	Lastname     string   `json:"lastname"`
+	Organization []string `json:"organization"`
+	Logins       int      `json:"logins"` //count the number if logins
+}
+
+type authToken struct {
+	Token string `json:"token"`
+}
+
+type userResponse struct {
+	*user
+	authToken
+}
+
+type errorResponse struct {
+	Error string `json:"error"`
+}
