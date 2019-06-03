1a3401fedb0e70021c71b6bfac54dd5e4bd94713
Jason Rich <jasonrich85@gmail.com>
2019-06-02 22:48:33 -0400

    added home and loggin classes

--- app/src/views/js/app.jsx
+++ app/src/views/js/app.jsx
@@ -7,3 +7,43 @@ class App extends React.Component {
     }
   }
 }
+
+class Home extends React.Componet {
+  render() {
+    return (
+      <div className="container">
+        <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
+          <h1>Learning Delivery Engine</h1>
+          <p>Sign in to get access </p>
+          <a onClick={this.authenticate} className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+class LoggedIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="col-lg-12">
+          <br />
+          <span className="pull-right"><a onClick={this.logout}>Log out</a></span>
+          <h2>Learning Delivery Engine</h2>
+          <p>A Revolutionary New Way to Delivery Homeschool Education!!!</p>
+          <div className="row">
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
